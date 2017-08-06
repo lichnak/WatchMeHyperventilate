96aaaa0670865c3521770aab219fb4bece20b058
Nick Tustison <ntustison@gmail.com>
2017-08-05 17:54:00 -0700

    ENH:  Add slides for Lisa.

--- Presentation/README.R
+++ Presentation/README.R
@@ -4,19 +4,30 @@
 #
 library(ggplot2)
 library(shiny)
-library(ggvis)
 library(rmarkdown)
 
 srcdir<-"./"
 buildrmd='wmhBuild.Rmd'
 
+# first presentation
+# rawrmds <- c( "format.Rmd",
+#               "antsBackground.Rmd",
+#               "wmh.Rmd"
+#                )
+
+# Second presentation:  August 11, 2017
 rawrmds <- c( "format.Rmd",
-              "antsBackground.Rmd",
               "wmh.Rmd"
                )
 
+
 for ( x in 1:length(rawrmds) ) {
-  if ( x == 1 )  cmd<-paste( "cat ",rawrmds[x]," > ",buildrmd ) else cmd<-paste( "cat ",rawrmds[x]," >> ",buildrmd )
+  if ( x == 1 )  
+    {
+    cmd<-paste( "cat ",rawrmds[x]," > ",buildrmd ) 
+    } else { 
+    cmd<-paste( "cat ",rawrmds[x]," >> ",buildrmd )
+    }
   system(cmd)
 }
 render(buildrmd, clean=TRUE )
